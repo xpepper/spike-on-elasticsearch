233c4b33662a27514b649cb006ea3ca727dda5fc
Pietro Di Bello <pietro.dibello@xpeppers.com>
2014-01-27 00:05:54 +0100

    final spike on uploading with a progress-bar

--- app/models/cover.rb
+++ app/models/cover.rb
@@ -6,4 +6,9 @@ class Cover < ActiveRecord::Base
 
   mount_uploader :image, ImageUploader
 
+  before_create :default_name
+
+  def default_name
+    self.title ||= File.basename(image.filename, '.*').titleize if image
+  end
 end
